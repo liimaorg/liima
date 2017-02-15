eloy83/agogo

Improved deployment view

AMW_rest/src/main/java/ch/mobi/itc/mobiliar/rest/resources/ResourceRelationsRest.java
@@ -35,6 +35,7 @@ import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.QueryParam;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -49,6 +50,9 @@ public class ResourceRelationsRest {
     @PathParam("releaseName")
     String releaseName;
 
+    @QueryParam("type")
+    String resourceType;
+
     @Inject
     PropertyEditor propertyEditor;
 
@@ -61,15 +65,15 @@ public class ResourceRelationsRest {
     @GET
     @ApiOperation(value = "Get all relations of the current resource")
     public List<ResourceRelationDTO> getResourceRelations() throws ValidationException {
-        return getResourceRelations(resourceGroupName, releaseName, false);
+        return getResourceRelations(resourceGroupName, releaseName, resourceType);
     }
 
-    List<ResourceRelationDTO> getResourceRelations(String resourceGroupName, String releaseName, boolean applicationsOnly) throws ValidationException {
+    List<ResourceRelationDTO> getResourceRelations(String resourceGroupName, String releaseName, String resourceType) throws ValidationException {
         ResourceEntity resource = resourceLocator.getResourceByNameAndReleaseWithRelations(resourceGroupName, releaseName);
         List<ResourceRelationDTO> resourceRelations = new ArrayList<>();
         for (ConsumedResourceRelationEntity relation : resource.getConsumedMasterRelations()) {
-            if (applicationsOnly && !relation.getResourceRelationType().getResourceTypeB().isApplicationResourceType()) {
-                    continue;
+            if (resourceType != null && !relation.getResourceRelationType().getResourceTypeB().getName().equals(resourceType)) {
+                continue;
             }
             resourceRelations.add(new ResourceRelationDTO(relation));
         }

liimaorg/liima

AMW_rest/src/main/java/ch/mobi/itc/mobiliar/rest/resources/ResourceRelationsRest.java
@@ -20,25 +20,23 @@
 
 package ch.mobi.itc.mobiliar.rest.resources;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.enterprise.context.RequestScoped;
-import javax.inject.Inject;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-
 import ch.mobi.itc.mobiliar.rest.dtos.ResourceRelationDTO;
 import ch.puzzle.itc.mobiliar.business.property.boundary.PropertyEditor;
 import ch.puzzle.itc.mobiliar.business.resourcegroup.boundary.ResourceLocator;
 import ch.puzzle.itc.mobiliar.business.resourcegroup.entity.ResourceEntity;
 import ch.puzzle.itc.mobiliar.business.resourcerelation.boundary.ResourceRelationLocator;
 import ch.puzzle.itc.mobiliar.business.resourcerelation.entity.ConsumedResourceRelationEntity;
-
 import ch.puzzle.itc.mobiliar.business.utils.ValidationException;
 import com.wordnik.swagger.annotations.Api;
 import com.wordnik.swagger.annotations.ApiOperation;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import java.util.ArrayList;
+import java.util.List;
 
 @RequestScoped
 @Path("/resources/{resourceGroupName}/{releaseName}/relations")
@@ -50,6 +48,9 @@ public class ResourceRelationsRest {
 
     @PathParam("releaseName")
     String releaseName;
+
+    @QueryParam("type")
+    String resourceType;
 
     @Inject
     PropertyEditor propertyEditor;
@@ -67,17 +68,17 @@ public class ResourceRelationsRest {
     @GET
     @ApiOperation(value = "Get all relations of the current resource")
     public List<ResourceRelationDTO> getResourceRelations() throws ValidationException {
-        return getResourceRelations(resourceGroupName, releaseName, false);
+        return getResourceRelations(resourceGroupName, releaseName, resourceType);
     }
 
-    List<ResourceRelationDTO> getResourceRelations(String resourceGroupName, String releaseName, boolean applicationsOnly) throws ValidationException {
+    List<ResourceRelationDTO> getResourceRelations(String resourceGroupName, String releaseName, String resourceType) throws ValidationException {
         ResourceEntity resource = resourceLocator.getResourceByNameAndReleaseWithRelations(resourceGroupName, releaseName);
         List<ResourceRelationDTO> resourceRelations = new ArrayList<>();
         for (ConsumedResourceRelationEntity relation : resource.getConsumedMasterRelations()) {
-			if (applicationsOnly && !relation.getResourceRelationType().getResourceTypeB().isApplicationResourceType()) {
-                    continue;
-            }            
-			ResourceRelationDTO resRel = new ResourceRelationDTO(relation);
+            if (resourceType != null && !relation.getResourceRelationType().getResourceTypeB().getName().equals(resourceType)) {
+                continue;
+            }
+            ResourceRelationDTO resRel = new ResourceRelationDTO(relation);
             List<TemplateDTO> templates = resourceRelationTemplatesRest.getResourceRelationTemplates(resourceGroupName, releaseName,
                     relation.getSlaveResource().getName(), relation.getSlaveResource().getRelease().getName(), "");
             addTemplates(resRel, templates);
@@ -85,8 +86,8 @@ public class ResourceRelationsRest {
         }
         return resourceRelations;
     }
-    
-   
+
+
     @Path("/{relatedResourceGroupName}")
     @GET
     @ApiOperation(value = "Get all related releases of the given resource")
@@ -109,7 +110,7 @@ public class ResourceRelationsRest {
     @GET
     @ApiOperation(value = "Get the relation between the two resource releases")
     public ResourceRelationDTO getResourceRelation(@PathParam("relatedResourceGroupName") String relatedResourceGroupName,
-            @PathParam("relatedReleaseName") String relatedReleaseName) throws ValidationException {
+                                                   @PathParam("relatedReleaseName") String relatedReleaseName) throws ValidationException {
         return new ResourceRelationDTO(resourceRelationLocator.getResourceRelation(resourceGroupName, releaseName,
                 relatedResourceGroupName, relatedReleaseName));
     }
@@ -151,5 +152,4 @@ public class ResourceRelationsRest {
         resRel.getTemplates().addAll(templatesToAdd);
     }
 
-    
 }