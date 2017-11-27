8555fc277db56149249bb23c06fcc79c5b871efc
Andy Pollari <pollari@puzzle.ch>
2017-11-27 08:53:27 +0100

    new DTO AuditViewEntry which represents a row in the AuditLogView

---
+++ AMW_business/src/main/java/ch/puzzle/itc/mobiliar/business/utils/AuditViewEntry.java
@@ -0,0 +1,15 @@
+package ch.puzzle.itc.mobiliar.business.utils;
+
+import lombok.Builder;
+import lombok.Getter;
+import org.hibernate.envers.RevisionType;
+
+@Getter
+@Builder
+public class AuditViewEntry {
+    long timestamp;
+    String type; // Property, Resource, ...
+    String user;
+    String value;
+    RevisionType mode;
+}
