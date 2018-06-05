0bc870a3d7939267ab6c07a5d937b753e4f93bee
Reto Galante <galante@puzzle.ch>
2018-06-05 13:12:38 +0200

    Added test

---
+++ AMW_rest/src/test/java/ch/mobi/itc/mobiliar/rest/resources/ResourcePropertiesRestTest.java
@@ -0,0 +1,41 @@
+package ch.mobi.itc.mobiliar.rest.resources;
+
+import ch.puzzle.itc.mobiliar.business.resourcegroup.boundary.ResourceLocator;
+import ch.puzzle.itc.mobiliar.business.utils.ValidationException;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import javax.ws.rs.core.Response;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class ResourcePropertiesRestTest {
+
+    @InjectMocks
+    ResourcePropertiesRest rest;
+
+    @Mock
+    ResourceLocator resourceLocator;
+
+    @Before
+    public void configure() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void shouldReturnNotFoundStatusIfNoPropertiesHaveBeenFound() throws ValidationException {
+        // given
+        int notFound = Response.Status.NOT_FOUND.getStatusCode();
+
+        // when
+        Response response = rest.getResourceProperties("Test");
+
+        // then
+        assertThat(response.getStatus(), is(notFound));
+    }
+
+}
