67f7ddee27c8c09550bfb6202d9246412d57e6a3
Reto Galante <galante@puzzle.ch>
2018-06-27 11:29:54 +0200

    Added body writer

---
+++ AMW_rest/src/main/java/ch/mobi/itc/mobiliar/rest/exceptions/ExceptionDtoBodyWriter.java
@@ -0,0 +1,49 @@
+package ch.mobi.itc.mobiliar.rest.exceptions;
+
+import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.ext.MessageBodyWriter;
+import javax.ws.rs.ext.Provider;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
+
+@Provider
+@Produces({"text/html"})
+public class ExceptionDtoBodyWriter implements MessageBodyWriter<ExceptionDto> {
+    @Override
+    public boolean isWriteable(Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType) {
+        if (ExceptionDto.class.isAssignableFrom(type)) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public long getSize(ExceptionDto exceptionDto, Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType) {
+        //used for contentLenght header, -1 = calculate it
+        return -1;
+    }
+
+    @Override
+    public void writeTo(ExceptionDto exceptionDto, Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType, MultivaluedMap<String, Object> httpHeaders, OutputStream entityStream) throws IOException, WebApplicationException {
+        PrintWriter pw = new PrintWriter(entityStream);
+
+        httpHeaders.add(HttpHeaders.CONTENT_ENCODING, "utf-8");
+        httpHeaders.add(HttpHeaders.CONTENT_TYPE, "text/html");
+
+        pw.append("<!DOCTYPE html>\n<html>");
+        pw.append("<head><title>Error</title></head><body>");
+        pw.append(exceptionDto.getMessage());
+        if (exceptionDto.getDetail() != null) {
+            pw.append("<br/>").append(exceptionDto.getDetail());
+        }
+        pw.append("</body></html>");
+        pw.flush();
+    }
+}
