ed91c7f470087d8371597beea0471969b2cc9345
Yves Peter <ypwebstuff@gmail.com>
2019-05-06 16:52:07 +0200

    remove maia web module

--- AMW_maia_federation/generated-sources/main/java/ch/mobi/xml/datatype/ch/mobi/maia/amw/maiaamwfederationservicetypes/v1_0/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://xml.mobi.ch/datatype/ch/mobi/maia/amw/MaiaAmwFederationServiceTypes/v1_0")
-package ch.mobi.xml.datatype.ch.mobi.maia.amw.maiaamwfederationservicetypes.v1_0;
