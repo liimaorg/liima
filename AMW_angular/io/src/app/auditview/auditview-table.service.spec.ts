42194419a7a1dd78d65c8efc2daac3b9449feaa4
Max Burri <max.burri@gmail.com>
2020-01-28 13:33:49 +0100

    correctly wire up DatePipe for AuditViewTableService

--- AMW_angular/io/src/app/auditview/auditview-table.service.spec.ts
+++ AMW_angular/io/src/app/auditview/auditview-table.service.spec.ts
@@ -1,9 +1,15 @@
+import { CommonModule, DatePipe } from '@angular/common';
 import { TestBed } from '@angular/core/testing';
-
 import { AuditviewTableService } from './auditview-table.service';
 
-describe('AuditviewTableService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+fdescribe('AuditviewTableService', () => {
+  beforeEach(() =>
+    TestBed.configureTestingModule({
+      imports: [CommonModule],
+      declarations: [],
+      providers: [DatePipe]
+    })
+  );
 
   it('should be created', () => {
     const service: AuditviewTableService = TestBed.get(AuditviewTableService);
