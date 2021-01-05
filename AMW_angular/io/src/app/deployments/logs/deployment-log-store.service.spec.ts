7a787f1befd1352c15729b907d03e4c41f63d5af
Max Burri <burri@puzzle.ch>
2021-01-05 08:18:54 +0100

    make tests work

--- AMW_angular/io/src/app/deployments/logs/deployment-log-store.service.spec.ts
+++ AMW_angular/io/src/app/deployments/logs/deployment-log-store.service.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { DeploymentLogStoreService } from './deployment-log-store.service';
@@ -6,7 +7,9 @@ describe('DeploymentLogStoreService', () => {
   let service: DeploymentLogStoreService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
     service = TestBed.inject(DeploymentLogStoreService);
   });
 
--- AMW_angular/io/src/app/deployments/logs/deployment-log.service.spec.ts
+++ AMW_angular/io/src/app/deployments/logs/deployment-log.service.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { DeploymentLogService } from './deployment-log.service';
@@ -6,7 +7,9 @@ describe('DeploymentLogService', () => {
   let service: DeploymentLogService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
     service = TestBed.inject(DeploymentLogService);
   });
 
