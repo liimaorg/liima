c8d283d0f9cf3d805305e5bace7131ec2ab231f9
Max Burri <burri@puzzle.ch>
2021-01-22 09:30:14 +0100

    more tests for coverage

--- AMW_web/src/test/java/ch/puzzle/itc/mobiliar/presentation/templateEdit/EditTemplateViewTest.java
+++ AMW_web/src/test/java/ch/puzzle/itc/mobiliar/presentation/templateEdit/EditTemplateViewTest.java
@@ -1,5 +1,6 @@
 package ch.puzzle.itc.mobiliar.presentation.templateEdit;
 
+import ch.puzzle.itc.mobiliar.business.shakedown.entity.ShakedownStpEntity;
 import ch.puzzle.itc.mobiliar.business.template.boundary.TemplateEditor;
 import ch.puzzle.itc.mobiliar.business.template.entity.TemplateDescriptorEntity;
 import ch.puzzle.itc.mobiliar.common.exception.AMWException;
@@ -20,13 +21,17 @@ public class EditTemplateViewTest {
     @Mock
     UserSettings settings;
 
-    @Mock
+    @Spy
     TemplateEditor templateEditor;
+
+    @Mock
+    ShakedownStpEntity selectedStp;
 
     @Spy
     @InjectMocks
     EditTemplateView editTemplateView;
 
+    @Spy
     private TemplateDescriptorEntity template;
 
     @Before
@@ -34,6 +39,7 @@ public class EditTemplateViewTest {
         doReturn(false).when(settings).isTestingMode();
         doReturn(true).when(editTemplateView).canModifyTemplates();
         doReturn(false).when(editTemplateView).fail(any(AMWException.class));
+        doNothing().when(editTemplateView).succeed();
 
         template = editTemplateView.getTemplate();
         template.setId(1);
@@ -75,7 +81,7 @@ public class EditTemplateViewTest {
         editTemplateView.save();
 
         // then
-        verify(editTemplateView).throwError("absolute paths are not allowed for file path");
+        verify(editTemplateView).throwError("Absolute paths are not allowed for file path");
         verify(editTemplateView, never()).succeed();
     }
 
@@ -88,7 +94,7 @@ public class EditTemplateViewTest {
         editTemplateView.save();
 
         // then
-        verify(editTemplateView).throwError("no path traversals like '../' allowed in file path");
+        verify(editTemplateView).throwError("No path traversals like '../' allowed in file path");
         verify(editTemplateView, never()).succeed();
     }
 
@@ -101,7 +107,100 @@ public class EditTemplateViewTest {
         editTemplateView.save();
 
         // then
-        verify(editTemplateView).throwError("no path traversals like '../' allowed in file path");
+        verify(editTemplateView).throwError("No path traversals like '../' allowed in file path");
         verify(editTemplateView, never()).succeed();
+    }
+
+    @Test
+    public void shouldRejectTemplateInTestingModeWihtoutSelectedSTP() throws AMWException {
+        // given
+        doReturn(true).when(settings).isTestingMode();
+        editTemplateView.setSelectedStpId(9999); // clear selectedStp in the view
+
+        // when
+        editTemplateView.save();
+
+        // then
+        verify(editTemplateView).throwError("No STP-name selected!");
+        verify(editTemplateView, never()).succeed();
+    }
+
+    @Test
+    public void shouldSetTemplateNameFromSelectedStp() throws AMWException {
+        // given
+        doReturn(true).when(settings).isTestingMode();
+        doReturn("name-of-selected-stp").when(selectedStp).getStpName();
+
+        editTemplateView.setResourceTypeId(10);
+        doNothing().when(templateEditor).saveTemplateForResourceType(template, 10);
+
+        // when
+        editTemplateView.save();
+
+        // then
+        verify(template).setName("name-of-selected-stp");
+        verify(editTemplateView).succeed();
+    }
+
+    @Test
+    public void shouldSaveTemplateForRelationWithRousourceIdNull() throws AMWException {
+        // given
+        editTemplateView.setRelationIdForTemplate(1);
+        editTemplateView.setResourceId(null);
+        doNothing().when(templateEditor).saveTemplateForRelation(template, 1, false);
+
+        // when
+        editTemplateView.save();
+
+        // then
+        verify(templateEditor).saveTemplateForRelation(template, 1, false);
+        verify(editTemplateView).succeed();
+
+    }
+
+    @Test
+    public void shouldSaveTemplateForRelationWithResourceId() throws AMWException {
+        editTemplateView.setRelationIdForTemplate(1);
+        editTemplateView.setResourceId(123);
+        doNothing().when(templateEditor).saveTemplateForRelation(template, 1, true);
+
+        // when
+        editTemplateView.save();
+
+        // then
+        verify(templateEditor).saveTemplateForRelation(template, 1, true);
+        verify(editTemplateView).succeed();
+    }
+
+    @Test
+    public void shouldSaveTemplateForResourceType() throws AMWException {
+        // given
+        editTemplateView.setRelationIdForTemplate(null);
+        editTemplateView.setResourceId(null);
+        editTemplateView.setResourceTypeId(99);
+        doNothing().when(templateEditor).saveTemplateForResourceType(template, 99);
+
+        // when
+        editTemplateView.save();
+
+        // then
+        verify(templateEditor).saveTemplateForResourceType(template, 99);
+        verify(editTemplateView).succeed();
+    }
+
+    @Test
+    public void shouldSaveTemplateForResource() throws AMWException {
+        // given
+        editTemplateView.setResourceTypeId(null);
+        editTemplateView.setRelationIdForTemplate(null);
+        editTemplateView.setResourceId(10);
+        doNothing().when(templateEditor).saveTemplateForResource(template, 10);
+
+        // when
+        editTemplateView.save();
+
+        // then
+        verify(templateEditor).saveTemplateForResource(template, 10);
+        verify(editTemplateView).succeed();
     }
 }
