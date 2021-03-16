467bd694f36130b397256d35ba3133966118a1c2
Simon Roth <roth@puzzle.ch>
2021-03-16 14:11:31 +0100

    fix tests, remove unused imports

--- AMW_angular/io/src/app/shared/date-time-picker/date-time.model.spec.ts
+++ AMW_angular/io/src/app/shared/date-time-picker/date-time.model.spec.ts
@@ -1,7 +1,7 @@
 import { TestBed } from '@angular/core/testing';
-import * as moment from 'moment';
 import { DateTimePickerComponent } from './date-time-picker.component';
-import { DateTimeModel } from "./date-time.model";
+import { DateTimeModel } from './date-time.model';
+import * as datefns from 'date-fns';
 
 describe('DateTimeModel', () => {
   it('should convert dates correctly', () => {
@@ -9,12 +9,12 @@ describe('DateTimeModel', () => {
     var component = fixture.componentInstance;
     var testDate = '02.01.2017 12:00';
 
-    var m = moment(testDate, component.dateStringFormat);
+    var m = datefns.parse(testDate, component.dateStringFormat, new Date());
     var dateTimeFromString = DateTimeModel.fromLocalString(testDate, component.dateStringFormat);
     var dateTimeFromEpoch = DateTimeModel.fromEpoch(m.valueOf());
 
-    expect(dateTimeFromString.day).toEqual(m.date());
-    expect(dateTimeFromString.month).toEqual(m.month() + 1);
+    expect(dateTimeFromString.day).toEqual(m.getDay());
+    expect(dateTimeFromString.month).toEqual(m.getMonth() + 1);
 
     expect(dateTimeFromString).toEqual(dateTimeFromEpoch);
 
