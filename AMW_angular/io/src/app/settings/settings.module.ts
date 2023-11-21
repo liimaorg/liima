e0e0f575a142dea606dec37650d5ab36100d3786
Clara Llorente <llorente@puzzle.ch>
2023-11-21 15:03:51 +0100

    feat: build tags input field, list and delete tags

--- AMW_angular/io/src/app/settings/settings.module.ts
+++ AMW_angular/io/src/app/settings/settings.module.ts
@@ -3,14 +3,17 @@ import { CommonModule } from '@angular/common';
 import {TagsComponent} from "./tags/tags.component";
 import {SettingsRoutingModule} from "./settings-routing.module";
 import {SettingsComponent} from "./settings.component";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 
 
 @NgModule({
   declarations: [SettingsComponent, TagsComponent],
-  imports: [
-    CommonModule,
-    SettingsRoutingModule
-  ]
+    imports: [
+        CommonModule,
+        SettingsRoutingModule,
+        ReactiveFormsModule,
+        FormsModule
+    ]
 })
 export class SettingsModule { }
--- AMW_angular/io/src/app/settings/tags/tags.component.ts
+++ AMW_angular/io/src/app/settings/tags/tags.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'amw-tags',
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrl: './tags.component.scss'
 })
 export class TagsComponent {
+  tagName: string = '';
+  tags: { id: number, name: string }[] = [];
+  private tagId = 0;
 
+  addTag(): void {
+    if (this.tagName.trim().length > 0) {
+      console.log('Tag added:', this.tagName);
+      this.tags.push({ id: this.tagId++, name: this.tagName.trim() });
+      console.log(this.tags);
+      this.tagName = '';
+    }
+  }
+
+  deleteTag(index: number): void {
+    this.tags.splice(index, 1);
+  }
 }
