292b7ff2c53dd17e30118bb1229f7f6783c72768
Max Burri <burri@puzzle.ch>
2023-11-22 20:27:30 +0100

    fix: import HttpClientTestingModule to fix the test

--- AMW_angular/io/src/app/settings/tags/tags.component.spec.ts
+++ AMW_angular/io/src/app/settings/tags/tags.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TagsComponent } from './tags.component';
+import {HttpClientTestingModule} from "@angular/common/http/testing";
 
 describe('TagsComponent', () => {
   let component: TagsComponent;
@@ -8,7 +9,8 @@ describe('TagsComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [TagsComponent]
+      declarations: [TagsComponent],
+      imports: [HttpClientTestingModule]
     })
     .compileComponents();
 
