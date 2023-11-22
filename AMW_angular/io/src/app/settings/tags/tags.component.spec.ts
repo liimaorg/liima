42df83066254298747334a6190e8a566143e1768
Max Burri <burri@puzzle.ch>
2023-11-22 20:28:37 +0100

    chore: reformat code with prettier

--- AMW_angular/io/src/app/settings/tags/tags.component.spec.ts
+++ AMW_angular/io/src/app/settings/tags/tags.component.spec.ts
@@ -1,7 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TagsComponent } from './tags.component';
-import {HttpClientTestingModule} from "@angular/common/http/testing";
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('TagsComponent', () => {
   let component: TagsComponent;
@@ -10,9 +10,8 @@ describe('TagsComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [TagsComponent],
-      imports: [HttpClientTestingModule]
-    })
-    .compileComponents();
+      imports: [HttpClientTestingModule],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(TagsComponent);
     component = fixture.componentInstance;
