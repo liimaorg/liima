f3ec1ec1f1f298aa4a7b40e9e4316a22ca505b4e
Max Burri <burri@puzzle.ch>
2023-12-14 14:21:16 +0100

    refactor: consistent naming for deployment routes

--- AMW_angular/io/src/app/app.routes.ts
+++ AMW_angular/io/src/app/app.routes.ts
@@ -2,8 +2,8 @@ import { Routes } from '@angular/router';
 import { DeploymentsComponent } from './deployments/deployments.component';
 import { auditviewRoutes } from './auditview/auditview.routes';
 import { deploymentRoutes } from './deployment/deployment-routes';
-import { deploymentsRoutes } from './deployments/deployments-routing.module';
 import { settingsRoutes } from './settings/settings.routes';
+import { deploymentsRoutes } from './deployments/deployments.routes';
 
 export const routes: Routes = [
   // default route only, the rest is done in module routing
