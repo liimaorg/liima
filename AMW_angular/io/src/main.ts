c257e652c2c9f454bd030ef1ba1406b23d4d31ce
Max Burri <burri@puzzle.ch>
2023-12-15 09:15:21 +0100

    refactor: no need to import providers - this is done in standalone components anyway

--- AMW_angular/io/src/main.ts
+++ AMW_angular/io/src/main.ts
@@ -2,13 +2,9 @@ import { enableProdMode, importProvidersFrom } from '@angular/core';
 
 import { environment } from './environments/environment';
 import { AppComponent } from './app/app.component';
-import { CodemirrorModule } from '@ctrl/ngx-codemirror';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { withInterceptorsFromDi, provideHttpClient } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
-import { NgSelectModule } from '@ng-select/ng-select';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter, withHashLocation } from '@angular/router';
 import { routes } from './app/app.routes';
 
@@ -18,7 +14,6 @@ if (environment.production) {
 
 bootstrapApplication(AppComponent, {
   providers: [
-    importProvidersFrom(BrowserModule, NgSelectModule, FormsModule, NgbModule, CodemirrorModule),
     provideRouter(routes, withHashLocation()),
     provideAnimations(),
     provideHttpClient(withInterceptorsFromDi()),
