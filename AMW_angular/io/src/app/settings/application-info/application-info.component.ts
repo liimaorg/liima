47e00788e73858bf78889a9dfb70775994f6d4a6
Max Burri <burri@puzzle.ch>
2024-01-05 08:58:13 +0100

    refactor: do not subscribe to observables in constructor and correctly handle isloading state

--- AMW_angular/io/src/app/settings/application-info/application-info.component.ts
+++ AMW_angular/io/src/app/settings/application-info/application-info.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 import { LoadingIndicatorComponent } from '../../shared/elements/loading-indicator.component';
 import { HttpClient } from '@angular/common/http';
-import { NgFor, NgIf } from '@angular/common';
+import { AsyncPipe, NgFor, NgIf } from '@angular/common';
+import { BehaviorSubject, forkJoin, mapTo, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 type Config = { key: { value: string; env: string }; value: string; defaultValue: string };
 type Version = { key: string; value: string };
@@ -9,25 +11,17 @@ type Version = { key: string; value: string };
 @Component({
   selector: 'amw-application-info',
   standalone: true,
-  imports: [LoadingIndicatorComponent, NgIf, NgFor],
+  imports: [LoadingIndicatorComponent, NgIf, NgFor, AsyncPipe],
   templateUrl: './application-info.component.html',
 })
 export class ApplicationInfoComponent {
-  isLoading: boolean = false;
-
-  appVersions: Version[] = [];
-  appConfigs: Config[] = [];
+  appVersions$: Observable<Version[]>;
+  appConfigs$: Observable<Config[]>;
+  isLoading$: Observable<boolean> = new BehaviorSubject<boolean>(true);
 
   constructor(private http: HttpClient) {
-    http.get<Version[]>('/AMW_rest/resources/settings/appInfo').subscribe({
-      next: (data) => {
-        this.appVersions = data;
-      },
-    });
-    http.get<Config[]>('/AMW_rest/resources/settings').subscribe({
-      next: (data) => {
-        this.appConfigs = data;
-      },
-    });
+    this.appVersions$ = http.get<Version[]>('/AMW_rest/resources/settings/appInfo');
+    this.appConfigs$ = http.get<Config[]>('/AMW_rest/resources/settings');
+    this.isLoading$ = forkJoin([this.appVersions$, this.appConfigs$]).pipe(map(() => false));
   }
 }
