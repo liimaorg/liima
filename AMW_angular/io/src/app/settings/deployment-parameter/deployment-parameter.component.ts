0ac7c8085a91eb074aa7250e2a1c0a5329de4f0d
llorentelemmc <llorente@puzzle.ch>
2024-01-31 16:13:22 +0100

    feat: display existing parameter keys

--- AMW_angular/io/src/app/settings/deployment-parameter/deployment-parameter.component.ts
+++ AMW_angular/io/src/app/settings/deployment-parameter/deployment-parameter.component.ts
@@ -1,13 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ToastComponent } from '../../shared/elements/toast/toast.component';
+import { HttpClient } from '@angular/common/http';
+
+type Key = { key: string; value: any };
 
 @Component({
   selector: 'app-deployment-parameter',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule, ToastComponent, FormsModule],
   templateUrl: './deployment-parameter.component.html',
-  styleUrl: './deployment-parameter.component.scss'
+  styleUrl: './deployment-parameter.component.scss',
 })
 export class DeploymentParameterComponent {
+  keyName = '';
+  paramKeys: Key[] = [];
 
+  @ViewChild(ToastComponent) toast: ToastComponent;
+
+  constructor(private http: HttpClient) {
+    http.get<Key[]>('/AMW_rest/resources/deployments/deploymentParameterKeys/').subscribe({
+      next: (data) => {
+        this.paramKeys = data;
+      },
+    });
+  }
+
+  addKey(): void {
+    if (this.keyName.trim().length > 0) {
+      // Dummy action: Display a toast message
+      this.toast.display('Dummy action: Key would be added.');
+      this.keyName = '';
+    }
+  }
+
+  deleteKey(keyId: number): void {
+    this.toast.display('Dummy action: Key would be deleted.');
+  }
 }
