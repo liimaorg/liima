09e74bd28efd35de29f78d8025f8a59ba959d358
Max Burri <burri@puzzle.ch>
2024-07-03 10:41:00 +0200

    feat: add a global error handler

--- AMW_angular/io/src/main.ts
+++ AMW_angular/io/src/main.ts
@@ -7,6 +7,7 @@ import { provideAnimations } from '@angular/platform-browser/animations';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter, withHashLocation } from '@angular/router';
 import { routes } from './app/app.routes';
+import { provideGlobalErrorHandler } from './app/shared/service/global-error.handler';
 
 if (environment.production) {
   enableProdMode();
@@ -17,5 +18,6 @@ bootstrapApplication(AppComponent, {
     provideRouter(routes, withHashLocation()),
     provideAnimations(),
     provideHttpClient(withInterceptorsFromDi()),
+    provideGlobalErrorHandler(),
   ],
 }).catch((err) => console.error(err));
