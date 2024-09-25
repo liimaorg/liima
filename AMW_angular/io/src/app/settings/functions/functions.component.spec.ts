b3df00316ab9086ce972875f38a8335f5913e6a3
llorentelemmc <llorente@puzzle.ch>
2024-09-25 09:54:43 +0200

    chore: fix tests

--- AMW_angular/io/src/app/settings/functions/functions.component.spec.ts
+++ AMW_angular/io/src/app/settings/functions/functions.component.spec.ts
@@ -1,20 +1,26 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FunctionsComponent } from './functions.component';
+import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
+import { HttpClient, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 describe('FunctionsComponent', () => {
   let component: FunctionsComponent;
   let fixture: ComponentFixture<FunctionsComponent>;
+  let httpTestingController: HttpTestingController;
+  let httpClient: HttpClient;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [FunctionsComponent]
-    })
-    .compileComponents();
+      imports: [FunctionsComponent],
+      providers: [provideHttpClient(withInterceptorsFromDi()), provideHttpClientTesting()],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(FunctionsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    httpTestingController = TestBed.inject(HttpTestingController);
+    httpClient = TestBed.inject(HttpClient);
   });
 
   it('should create', () => {
