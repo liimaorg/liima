a2bb542a24ec25ac1c712d0bb91ad2f6d7f3006b
Max Burri <burri@puzzle.ch>
2024-10-16 13:45:13 +0200

    feat(ui): add cy-data to page title

--- AMW_angular/io/src/app/layout/page/page.component.ts
+++ AMW_angular/io/src/app/layout/page/page.component.ts
@@ -10,7 +10,7 @@ import { RouterLink } from '@angular/router';
   template: `
     <div class="row">
       <div class="col-12 bg-dark-subtle pt-2">
-        <h4><ng-content select=".page-title"></ng-content></h4>
+        <h4 data-cy="page-title"><ng-content select=".page-title"></ng-content></h4>
       </div>
     </div>
     <div class="container mt-4">
