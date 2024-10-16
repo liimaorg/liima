a0b26035df3feba7fd98af4fa82107a5b45da728
Max Burri <burri@puzzle.ch>
2024-10-16 15:06:29 +0200

    feat(ui): show list of servers
    use hard coded example data

---
+++ AMW_angular/io/src/app/servers/servers-list/servers-list.component.ts
@@ -0,0 +1,52 @@
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { AppsListComponent } from '../../apps/apps-list/apps-list-component';
+
+export type Server = {
+  host: string;
+  environment: string;
+  appServer: string;
+  appServerRelease: string;
+  runtime: string;
+  node: string;
+  nodeRelease: string;
+};
+
+@Component({
+  selector: 'app-servers-list',
+  standalone: true,
+  imports: [AppsListComponent],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  template: `<div class="table-responsive">
+    @if (servers() && servers().length > 0) {
+    <table class="table table-sm table-borderless">
+      <thead class="table-light">
+        <tr>
+          <th>Host</th>
+          <th>Env</th>
+          <th>AppServer</th>
+          <th>AppServer Release</th>
+          <th>Runtime</th>
+          <th>Node</th>
+          <th>Node Release</th>
+        </tr>
+      </thead>
+      <tbody>
+        @for (server of servers(); track server; let even = $even) {
+        <tr [class.table-light]="!even">
+          <td>{{ server.host }}</td>
+          <td>{{ server.environment }}</td>
+          <td>{{ server.appServer }}</td>
+          <td>{{ server.appServerRelease }}</td>
+          <td>{{ server.runtime }}</td>
+          <td>{{ server.node }}</td>
+          <td>{{ server.nodeRelease }}</td>
+        </tr>
+        }
+      </tbody>
+    </table>
+    }
+  </div>`,
+})
+export class ServersListComponent {
+  servers = input.required<Server[]>();
+}
--- AMW_angular/io/src/app/servers/servers-page.component.ts
+++ AMW_angular/io/src/app/servers/servers-page.component.ts
@@ -2,19 +2,20 @@ import { ChangeDetectionStrategy, Component, computed, inject, signal } from '@a
 import { PageComponent } from '../layout/page/page.component';
 import { LoadingIndicatorComponent } from '../shared/elements/loading-indicator.component';
 import { AuthService } from '../auth/auth.service';
+import { Server, ServersListComponent } from './servers-list/servers-list.component';
 
 @Component({
   selector: 'app-servers-page',
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [PageComponent, LoadingIndicatorComponent],
+  imports: [PageComponent, LoadingIndicatorComponent, ServersListComponent],
   template: ` <app-loading-indicator [isLoading]="isLoading()"></app-loading-indicator>
     <app-page>
       <div class="page-title">Servers</div>
       <div class="page-content">
         {{ permissions() }}
-      </div></app-page
-    >`,
+        <app-servers-list [servers]="servers()" /></div
+    ></app-page>`,
 })
 export class ServersPageComponent {
   private authService = inject(AuthService);
@@ -30,4 +31,26 @@ export class ServersPageComponent {
       return { canViewSomething: false };
     }
   });
+
+  servers = signal<Server[]>([
+    {
+      host: 'host1',
+      environment: 'A',
+      appServer: 'Application Server 1',
+      appServerRelease: 'multiple',
+      runtime: 'multiple runtimes',
+      node: 'node1',
+      nodeRelease: '1.1',
+    },
+    {
+      host: 'host2',
+      environment: 'B',
+      appServer: 'Application Server 2',
+      appServerRelease: 'multiple',
+      runtime: 'multiple runtimes',
+      node: 'node2',
+      nodeRelease: '1.2',
+    },
+  ]);
+
 }
