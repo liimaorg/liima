bbd12a06cdccee1c1aaeaf220584c2b66d7fba0b
Max Burri <burri@puzzle.ch>
2024-10-17 08:51:16 +0200

    feat(ui): get url to link from environment variables

--- AMW_angular/io/src/app/core/amw-constants.ts
+++ AMW_angular/io/src/app/core/amw-constants.ts
@@ -1,4 +1,9 @@
 export const AMW_LOGOUT_URL = 'amw.logoutUrl';
+export const ENVIRONMENT = {
+  AMW_VM_DETAILS_URL: 'amw.vmDetailUrl',
+  AMW_VM_URL_PARAM: 'amw.vmUrlParam',
+};
+
 // used for date-fns
 export const DATE_TIME_FORMAT = 'dd.MM.yyyy HH:mm';
 export const DATE_FORMAT = 'dd.MM.yyyy';
--- AMW_angular/io/src/app/servers/servers-page.component.ts
+++ AMW_angular/io/src/app/servers/servers-page.component.ts
@@ -1,9 +1,11 @@
-import { ChangeDetectionStrategy, Component, computed, inject, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, inject, Signal, signal } from '@angular/core';
 import { PageComponent } from '../layout/page/page.component';
 import { LoadingIndicatorComponent } from '../shared/elements/loading-indicator.component';
 import { AuthService } from '../auth/auth.service';
 import { ServersListComponent } from './servers-list/servers-list.component';
 import { ServersService } from './servers.service';
+import { Config, ConfigurationService } from '../shared/service/configuration.service';
+import { ENVIRONMENT } from '../core/amw-constants';
 
 @Component({
   selector: 'app-servers-page',
@@ -15,16 +17,34 @@ import { ServersService } from './servers.service';
       <div class="page-title">Servers</div>
       <div class="page-content">
         {{ loadingPermissions() }}
-        <app-servers-list [servers]="servers()" /></div
+        <app-servers-list
+          [servers]="servers()"
+          [canReadAppServer]="canReadAppServer"
+          [canReadResources]="canReadResources"
+          [linkToHostUrl]="linkToHostUrl()"
+        /></div
     ></app-page>`,
 })
 export class ServersPageComponent {
   private authService = inject(AuthService);
   private serversService = inject(ServersService);
+  private configurationService = inject(ConfigurationService);
+
+  canReadAppServer: boolean;
+  canReadResources: boolean;
 
   isLoading = signal(false);
 
   servers = this.serversService.servers;
+  configuration: Signal<Config[]> = this.configurationService.configuration;
+
+  linkToHostUrl = computed(() => {
+    if (!this.configuration()) return;
+    const config = this.configuration();
+    const vmDetailUrl = config.filter((c) => c.key.value === ENVIRONMENT.AMW_VM_DETAILS_URL).map((c) => c.value)[0];
+    const vmUrlParam = config.filter((c) => c.key.value === ENVIRONMENT.AMW_VM_URL_PARAM).map((c) => c.value)[0];
+    return `${vmDetailUrl}?${vmUrlParam}=`;
+  });
 
   loadingPermissions = computed(() => {
     if (this.authService.restrictions().length > 0) {
@@ -34,5 +54,8 @@ export class ServersPageComponent {
     }
   });
 
-  private getUserPermissions() {}
+  private getUserPermissions() {
+    this.canReadAppServer = this.authService.hasResourcePermission('RESOURCE', 'CREATE', 'APPLICATION');
+    this.canReadResources = this.authService.hasPermission('RESOURCE', 'READ');
+  }
 }
