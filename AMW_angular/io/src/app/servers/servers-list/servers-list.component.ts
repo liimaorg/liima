e5ee40e373cff7c09e2799bb1f4dc553d6cf2fcb
Max Burri <burri@puzzle.ch>
2024-10-17 08:51:16 +0200

    feat(ui): get url to link from environment variables

--- AMW_angular/io/src/app/servers/servers-list/servers-list.component.ts
+++ AMW_angular/io/src/app/servers/servers-list/servers-list.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, input, Signal } from '@angular/core';
 import { AppsListComponent } from '../../apps/apps-list/apps-list-component';
 import { Server } from '../server';
 
@@ -25,19 +25,29 @@ import { Server } from '../server';
         @for (server of servers(); track server; let even = $even) {
         <tr [class.table-light]="!even">
           <td>{{ server.host }}</td>
+          <td>
+            <a href="{{ linkToHostUrl() }}={{ server.host }}">{{ server.host }}</a>
+          </td>
           <td>{{ server.environment }}</td>
           <td>
+            @if(canReadAppServer()) {
             <a
               href="/AMW_web/pages/editResourceView.xhtml?ctx={{ server.environmentId }}&id={{ server.appServerId }}"
               >{{ server.appServer }}</a
             >
+            } @else {
+            {{ server.appServer }} }
           </td>
           <td>{{ server.appServerRelease }}</td>
           <td>{{ server.runtime }}</td>
           <td>
+            @if (canReadResources()) {
             <a href="/AMW_web/pages/editResourceView.xhtml?ctx={{ server.environmentId }}&id={{ server.nodeId }}">{{
               server.node
             }}</a>
+            } @else {
+            {{ server.node }}
+            }
           </td>
           <td>{{ server.nodeRelease }}</td>
         </tr>
@@ -49,4 +59,7 @@ import { Server } from '../server';
 })
 export class ServersListComponent {
   servers = input.required<Server[]>();
+  canReadAppServer = input.required<boolean>();
+  canReadResources = input.required<boolean>();
+  linkToHostUrl = input.required<string>();
 }
--- AMW_angular/io/src/app/servers/servers-page.component.ts
+++ AMW_angular/io/src/app/servers/servers-page.component.ts
@@ -1,9 +1,11 @@
-import { ChangeDetectionStrategy, Component, computed, inject, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, inject, Signal, signal } from '@angular/core';
 import { PageComponent } from '../layout/page/page.component';
 import { LoadingIndicatorComponent } from '../shared/elements/loading-indicator.component';
 import { AuthService } from '../auth/auth.service';
 import { ServersListComponent } from './servers-list/servers-list.component';
 import { ServersService } from './servers.service';
+import { Config, ConfigurationService } from '../shared/service/configuration.service';
+import { ENVIRONMENT } from '../core/amw-constants';
 
 @Component({
   selector: 'app-servers-page',
@@ -15,24 +17,47 @@ import { ServersService } from './servers.service';
       <div class="page-title">Servers</div>
       <div class="page-content">
         {{ permissions() }}
-        <app-servers-list [servers]="servers()" /></div
+
+        <app-servers-list
+          [servers]="servers()"
+          [canReadAppServer]="canReadAppServer"
+          [canReadResources]="canReadResources"
+          [linkToHostUrl]="linkToHostUrl()"
+        /></div
     ></app-page>`,
 })
 export class ServersPageComponent {
   private authService = inject(AuthService);
   private serversService = inject(ServersService);
+  private configurationService = inject(ConfigurationService);
+
+  canReadAppServer: boolean;
+  canReadResources: boolean;
 
   isLoading = signal(false);
 
   servers = this.serversService.servers;
+  configuration: Signal<Config[]> = this.configurationService.configuration;
+
+  linkToHostUrl = computed(() => {
+    if (!this.configuration()) return;
+    const config = this.configuration();
+    const vmDetailUrl = config.filter((c) => c.key.value === ENVIRONMENT.AMW_VM_DETAILS_URL).map((c) => c.value)[0];
+    const vmUrlParam = config.filter((c) => c.key.value === ENVIRONMENT.AMW_VM_URL_PARAM).map((c) => c.value)[0];
+    return `${vmDetailUrl}?${vmUrlParam}=`;
+  });
 
   permissions = computed(() => {
     if (this.authService.restrictions().length > 0) {
       return {
-        canViewSomething: true,
+        canReadAppServer: this.authService.hasResourcePermission('RESOURCE', 'CREATE', 'APPLICATION'),
+        canReadResources: this.authService.hasPermission('RESOURCE', 'READ'),
       };
     } else {
-      return { canViewSomething: false };
+      return {
+        canReadAppServer: false,
+        canReadResources: false,
+      };
     }
   });
 }
