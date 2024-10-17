467d66618127d989fb0d96e48ec3b8d17c6c902c
Max Burri <burri@puzzle.ch>
2024-10-17 07:14:36 +0200

    feat(ui): correct user credentials, assert application is shown in table

--- AMW_e2e/cypress/e2e/servers/servers.cy.js
+++ AMW_e2e/cypress/e2e/servers/servers.cy.js
@@ -1,10 +1,13 @@
 describe("Servers Page", () => {
   it("should navigate to the servers page", () => {
     cy.visit("AMW_angular/#/servers", {
-      username: "admin",
-      password: "admin",
+      auth: {
+        username: "admin",
+        password: "admin",
+      },
     });
 
     cy.get('[data-cy="page-title"]').contains("Servers");
+    cy.get("tbody > tr > :nth-child(3)").contains("testapplicationserver");
   });
 });
