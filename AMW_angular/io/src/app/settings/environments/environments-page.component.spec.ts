62fcdb7f37a7472996291746e82ecd7d5152fa68
Berivan Bertagna <bbertagna@puzzle.ch>
2024-10-21 14:14:31 +0200

    fix: tests

--- AMW_angular/io/src/app/settings/environments/environments-page.component.spec.ts
+++ AMW_angular/io/src/app/settings/environments/environments-page.component.spec.ts
@@ -1,5 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { EnvironmentsPageComponent } from './environments-page.component';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 describe('EnvironmentsPageComponent', () => {
   let component: EnvironmentsPageComponent;
@@ -8,6 +10,7 @@ describe('EnvironmentsPageComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [EnvironmentsPageComponent],
+      providers: [provideHttpClient(withInterceptorsFromDi()), provideHttpClientTesting()],
     }).compileComponents();
 
     fixture = TestBed.createComponent(EnvironmentsPageComponent);
