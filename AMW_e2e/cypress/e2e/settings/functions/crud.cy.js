7641df864a79ccfa0bb0e27c24592a101542aa04
Max Burri <burri@puzzle.ch>
2024-10-23 17:53:34 +0200

    fix(e2e): make tests work

--- AMW_e2e/cypress/e2e/settings/functions/crud.cy.js
+++ AMW_e2e/cypress/e2e/settings/functions/crud.cy.js
@@ -17,36 +17,27 @@ describe("Functions -CRUD", () => {
       },
     });
 
-   cy.get('[data-cy="button-add"]').click();
-   cy.get("#name").type("testFunction");
-   cy.get('[data-cy="button-save"]').should("be.disabled");
-   cy.get('.CodeMirror-line').type("testContent");
-   cy.get('[data-cy="button-save"]').click();
-   cy.wait(5000);
-   cy.get('[data-cy="button-add"]').click();
-   cy.get("#name").type("testFunction");
-   cy.get('.CodeMirror-line').type("differentContent");
-   cy.get('[data-cy="button-save"]').click()
-   cy.contains("Function with same name already exists");
-   cy.wait(15000);
-   cy.get('[data-cy="button-add"]').click();
-   cy.get('.CodeMirror-line').type("testContent");
-   cy.get('[data-cy="button-save"]').should("be.disabled");
-   cy.get("#name").type("differentFunction");
-   cy.get('[data-cy="button-save"]').click();
-   cy.get('[data-cy="button-delete"]').click();
-/*    cy.get("#tagName").should("be.empty");
-    cy.contains("Tag added");
-    cy.get("#tagName").type("test-tag{enter}");
-    cy.get("#tagName").should("be.empty");
-    cy.contains("Tag with name test-tag already exists");
-    cy.get("#tagName").clear();
-    cy.get("#tagName").type("other-tag");
     cy.get('[data-cy="button-add"]').click();
-    cy.get("#tagName").should("be.empty");
-    cy.get('[data-cy="delete-other-tag"]').click();
-    cy.contains("Tag deleted");
-    cy.get('[data-cy="delete-test-tag"]').click();
-    cy.contains("Tag deleted");*/
+    cy.get("#name").type("testFunction");
+    cy.get('[data-cy="button-save"]').should("be.disabled");
+    cy.get(".CodeMirror-line").type("testContent");
+    cy.get('[data-cy="button-save"]').click();
+    cy.contains("Function saved successfully.");
+    cy.get('[data-cy="button-add"]').click();
+    cy.get("#name").type("testFunction");
+    cy.get(".CodeMirror-line").type("differentContent");
+    cy.get('[data-cy="button-save"]').click({ force: true });
+    cy.contains("Function with same name already exists");
+    cy.get('[data-cy="button-add"]').click();
+    cy.get(".CodeMirror-line").type("testContent");
+    cy.get('[data-cy="button-save"]').should("be.disabled");
+    cy.get("#name").type("differentFunction");
+    cy.get('[data-cy="button-save"]').click({ force: true });
+    cy.get('[data-cy="button-delete-1"]').click();
+    cy.get('[data-cy="button-delete"]').click();
+    cy.contains("Function deleted");
+    cy.get('[data-cy="button-delete-0"]').click();
+    cy.get('[data-cy="button-delete"]').click();
+    cy.contains("Function deleted");
   });
 });
