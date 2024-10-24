a9fca184b3614c4b36e9750ac28a334245605f04
Berivan Bertagna <bbertagna@puzzle.ch>
2024-10-24 10:32:15 +0200

    feat: add new endpoint to add new environment

--- AMW_rest/src/main/java/ch/mobi/itc/mobiliar/rest/environments/EnvironmentsRest.java
+++ AMW_rest/src/main/java/ch/mobi/itc/mobiliar/rest/environments/EnvironmentsRest.java
@@ -2,19 +2,24 @@ package ch.mobi.itc.mobiliar.rest.environments;
 
 import ch.mobi.itc.mobiliar.rest.dtos.EnvironmentDTO;
 import ch.puzzle.itc.mobiliar.business.environment.boundary.ContextLocator;
+import ch.puzzle.itc.mobiliar.business.environment.control.EnvironmentsScreenDomainService;
 import ch.puzzle.itc.mobiliar.business.environment.entity.ContextEntity;
+import ch.puzzle.itc.mobiliar.common.exception.ElementAlreadyExistsException;
+import ch.puzzle.itc.mobiliar.common.exception.ResourceNotFoundException;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.QueryParam;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Response;
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import static javax.ws.rs.core.Response.Status.*;
 
 @RequestScoped
 @Path("/environments")
@@ -23,6 +28,9 @@ public class EnvironmentsRest {
 
     @Inject
     ContextLocator contextLocator;
+
+    @Inject
+    EnvironmentsScreenDomainService environmentsScreenDomainService;
 
     @GET
     @ApiOperation(value = "Get environments", notes = "Returns the available environments")
@@ -50,4 +58,14 @@ public class EnvironmentsRest {
         return contextLocator.getAllEnvironments().stream().map(EnvironmentDTO::new).collect(Collectors.toList());
     }
 
+    @POST
+    @Path("/contexts")
+    @ApiOperation(value = "Add a context")
+    public Response addContext(@ApiParam() EnvironmentDTO request) throws ElementAlreadyExistsException, ResourceNotFoundException {
+        //TODO: EnvironmentsScreenDomainService is deprecated.
+        ContextEntity newContextEntity = environmentsScreenDomainService.createContextByName(request.getName(), request.getParentId());
+        return Response.status(CREATED)
+                .location(URI.create("/settings/environments/contexts" + newContextEntity.getId()))
+                .build();
+    }
 }
