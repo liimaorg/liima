ab944a777a9a33fdbc3f755d28ecf0fcf315e28b
llorentelemmc <llorente@puzzle.ch>
2024-10-30 14:49:38 +0100

    chore: adjust function-delete component

--- AMW_angular/io/src/app/settings/functions/function-delete.component.ts
+++ AMW_angular/io/src/app/settings/functions/function-delete.component.ts
@@ -2,12 +2,13 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { AppFunction } from './appFunction';
 import { ModalHeaderComponent } from '../../shared/modal-header/modal-header.component';
+import { ButtonComponent } from '../../shared/button/button.component';
 
 @Component({
   selector: 'app-function-delete',
   standalone: true,
   templateUrl: './function-delete.component.html',
-  imports: [ModalHeaderComponent],
+  imports: [ModalHeaderComponent, ButtonComponent],
 })
 export class FunctionDeleteComponent {
   @Input() function: AppFunction;
