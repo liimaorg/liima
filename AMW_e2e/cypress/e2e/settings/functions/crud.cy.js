1af4e397db7e4f38be85b1155b2c05ee7513533d
llorentelemmc <llorente@puzzle.ch>
2024-10-30 12:53:59 +0100

    chore: fix e2e tests

--- AMW_e2e/cypress/e2e/settings/functions/crud.cy.js
+++ AMW_e2e/cypress/e2e/settings/functions/crud.cy.js
@@ -17,27 +17,27 @@ describe("Functions -CRUD", () => {
       },
     });
 
-    cy.get('[data-cy="button-add"]').click();
+    cy.get('[data-cy="button-add"]').click({ force: true });
     cy.get("#name").type("testFunction");
-    cy.get('[data-cy="button-save"]').should("be.disabled");
+    cy.get('[data-cy="button-save"]').should('have.css', 'pointer-events', 'none');
     cy.get(".CodeMirror-line").type("testContent");
-    cy.get('[data-cy="button-save"]').click();
+    cy.get('[data-cy="button-save"]').click({ force: true });
     cy.contains("Function saved successfully.");
-    cy.get('[data-cy="button-add"]').click();
+    cy.get('[data-cy="button-add"]').click({ force: true });
     cy.get("#name").type("testFunction");
     cy.get(".CodeMirror-line").type("differentContent");
     cy.get('[data-cy="button-save"]').click({ force: true });
     cy.contains("Function with same name already exists");
-    cy.get('[data-cy="button-add"]').click();
-    cy.get(".CodeMirror-line").type("testContent");
-    cy.get('[data-cy="button-save"]').should("be.disabled");
+    cy.get('[data-cy="button-add"]').click({ force: true });
+    cy.get(".CodeMirror-line").first().type("testContent");
+    cy.get('[data-cy="button-save"]').should('have.css', 'pointer-events', 'none');
     cy.get("#name").type("differentFunction");
     cy.get('[data-cy="button-save"]').click({ force: true });
-    cy.get('[data-cy="button-delete-1"]').click();
-    cy.get('[data-cy="button-delete"]').click();
+    cy.get('[data-cy="button-delete-1"]').click({ force: true });
+    cy.get('[data-cy="button-delete"]').click({ force: true });
     cy.contains("Function deleted");
-    cy.get('[data-cy="button-delete-0"]').click();
-    cy.get('[data-cy="button-delete"]').click();
+    cy.get('[data-cy="button-delete-0"]').click({ force: true });
+    cy.get('[data-cy="button-delete"]').click({ force: true });
     cy.contains("Function deleted");
   });
 });
