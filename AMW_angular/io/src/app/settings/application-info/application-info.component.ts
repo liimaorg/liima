52c88666c0cdb87bd11ee7de741d1b9237e2e438
llorentelemmc <llorente@puzzle.ch>
2024-11-02 20:39:50 +0100

    refactor: directly assign Observables

--- AMW_angular/io/src/app/settings/application-info/application-info.component.ts
+++ AMW_angular/io/src/app/settings/application-info/application-info.component.ts
@@ -1,27 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { LoadingIndicatorComponent } from '../../shared/elements/loading-indicator.component';
 import { HttpClient } from '@angular/common/http';
 import { AsyncPipe } from '@angular/common';
-import { BehaviorSubject, forkJoin, Observable } from 'rxjs';
+import { forkJoin } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 type Config = { key: { value: string; env: string }; value: string; defaultValue: string };
 type Version = { key: string; value: string };
 
 @Component({
-  selector: 'amw-application-info',
+  selector: 'app-application-info',
   standalone: true,
   imports: [LoadingIndicatorComponent, AsyncPipe],
   templateUrl: './application-info.component.html',
 })
 export class ApplicationInfoComponent {
-  appVersions$: Observable<Version[]>;
-  appConfigs$: Observable<Config[]>;
-  isLoading$: Observable<boolean> = new BehaviorSubject<boolean>(true);
-
-  constructor(private http: HttpClient) {
-    this.appVersions$ = http.get<Version[]>('/AMW_rest/resources/settings/appInfo');
-    this.appConfigs$ = http.get<Config[]>('/AMW_rest/resources/settings');
-    this.isLoading$ = forkJoin([this.appVersions$, this.appConfigs$]).pipe(map(() => false));
-  }
+  private http = inject(HttpClient);
+  appVersions$ = this.http.get<Version[]>('/AMW_rest/resources/settings/appInfo');
+  appConfigs$ = this.http.get<Config[]>('/AMW_rest/resources/settings');
+  isLoading$ = forkJoin([this.appVersions$, this.appConfigs$]).pipe(map(() => false));
 }
