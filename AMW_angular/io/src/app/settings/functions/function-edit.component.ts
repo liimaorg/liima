6c1b1e40bd0bc1a83aba23607dced9e481255026
Stephan Girod <girod@puzzle.ch>
2024-11-06 17:54:03 +0100

    feat: add diff-component and fullscreen-toggle #1022

--- AMW_angular/io/src/app/settings/functions/function-edit.component.ts
+++ AMW_angular/io/src/app/settings/functions/function-edit.component.ts
@@ -3,19 +3,20 @@ import { FormsModule } from '@angular/forms';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { CommonModule } from '@angular/common';
 import { AppFunction } from './appFunction';
-//import { CodemirrorModule } from '@ctrl/ngx-codemirror';
 import { FunctionsService } from './functions.service';
 import { RevisionInformation } from './revisionInformation';
 import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 import { ButtonComponent } from '../../shared/button/button.component';
 import { ModalHeaderComponent } from '../../shared/modal-header/modal-header.component';
 import { CodeEditorComponent } from '../../shared/codemirror/code-editor.component';
+import { DiffEditor } from '../../shared/codemirror/diff-editor.component';
+import { IconComponent } from '../../shared/icon/icon.component';
 
 @Component({
   selector: 'app-function-edit',
   templateUrl: './function-edit.component.html',
   standalone: true,
-  imports: [FormsModule, CommonModule, NgbDropdownModule, ModalHeaderComponent, ButtonComponent],
+  imports: [CodeEditorComponent, DiffEditor, FormsModule, CommonModule, IconComponent, NgbDropdownModule, ModalHeaderComponent, ButtonComponent],
 })
 export class FunctionEditComponent implements OnInit {
   @Input() function: AppFunction;
@@ -24,8 +25,15 @@ export class FunctionEditComponent implements OnInit {
 
   private functionsService = inject(FunctionsService);
   public revisions: RevisionInformation[] = [];
-  private revision: AppFunction;
+  public revision: AppFunction;
   public selectedRevisionName: string;
+  public isFullscreen = false;
+  public toggleFullscreenIcon = 'arrows-fullscreen';
+
+  public diffValue = {
+    original: '',
+    modified: '',
+  };
 
   constructor(public activeModal: NgbActiveModal) {}
 
@@ -45,6 +53,7 @@ export class FunctionEditComponent implements OnInit {
   }
 
   save() {
+    if (this.revision) this.function.content = this.diffValue.original;
     this.saveFunction.emit(this.function);
     this.activeModal.close();
   }
@@ -59,10 +68,13 @@ export class FunctionEditComponent implements OnInit {
     this.functionsService.getFunctionByIdAndRevision(functionId, revisionId).subscribe((revision) => {
       this.revision = revision;
       this.selectedRevisionName = displayName;
+      this.diffValue = { original: this.function.content, modified: this.revision.content };
     });
   }
 
-  onChange(content: string) {
-    this.function = { ...this.function, content: content };
+  toggleFullscreen() {
+    this.isFullscreen = !this.isFullscreen;
+    this.toggleFullscreenIcon = this.isFullscreen ? 'fullscreen-exit' : 'arrows-fullscreen';
+    this.activeModal.update({ fullscreen: this.isFullscreen });
   }
 }
