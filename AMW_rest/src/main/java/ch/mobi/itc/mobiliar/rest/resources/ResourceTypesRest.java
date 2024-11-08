ab4b153499847ac94355f469a837434e58e7c211
llorentelemmc <llorente@puzzle.ch>
2024-11-08 16:28:31 +0100

    chore: restructure list of root resource types

--- AMW_rest/src/main/java/ch/mobi/itc/mobiliar/rest/resources/ResourceTypesRest.java
+++ AMW_rest/src/main/java/ch/mobi/itc/mobiliar/rest/resources/ResourceTypesRest.java
@@ -27,7 +27,6 @@ import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 
 import ch.mobi.itc.mobiliar.rest.dtos.ResourceTypeDTO;
 import ch.puzzle.itc.mobiliar.business.property.boundary.PropertyEditor;
@@ -71,12 +70,5 @@ public class ResourceTypesRest {
         return resourceTypeLocator.getRootResourceTypes().stream()
                 .map(ResourceTypeDTO::new)
                 .collect(Collectors.toList());
-    }
-
-    @Path("/resourceTypes/{resourceTypeId}/hasChildren")
-    @GET
-    @ApiOperation(value = "Checks if resource type has children")
-    public boolean hasChildren(@PathParam("resourceTypeId") int resourceTypeId) {
-        return resourceTypeLocator.hasChildren(resourceTypeId);
     }
 }
