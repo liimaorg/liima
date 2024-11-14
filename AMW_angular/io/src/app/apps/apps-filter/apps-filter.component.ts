5376e882c7e0db1ff032c3b37df73cad07e51c7e
Stephan Girod <girod@puzzle.ch>
2024-11-14 11:52:00 +0100

    fix: use only selected signal to bind in template

--- AMW_angular/io/src/app/apps/apps-filter/apps-filter.component.ts
+++ AMW_angular/io/src/app/apps/apps-filter/apps-filter.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, computed, input, Input, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, input, Input, OnDestroy, OnInit, signal } from '@angular/core';
 
 import { NgSelectModule } from '@ng-select/ng-select';
 import { Release } from '../../settings/releases/release';
@@ -6,6 +6,9 @@ import { FormsModule } from '@angular/forms';
 import { Output, EventEmitter } from '@angular/core';
 import { ButtonComponent } from '../../shared/button/button.component';
 import { IconComponent } from '../../shared/icon/icon.component';
+import { toObservable } from '@angular/core/rxjs-interop';
+import { skip, Subject, take } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-apps-filter',
@@ -14,22 +17,30 @@ import { IconComponent } from '../../shared/icon/icon.component';
   templateUrl: './apps-filter.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppsFilterComponent {
+export class AppsFilterComponent implements OnDestroy {
   @Input() releases: Release[];
 
-  upcoming = input<number>();
+  private destroy$ = new Subject<void>();
+  upcoming = input.required<number>();
   selected = signal<number>(undefined);
-  pristine: boolean = true;
 
   @Output() filterEvent = new EventEmitter<{ filter: string; releaseId: number }>();
 
   appName: string;
 
+  constructor() {
+    toObservable(this.upcoming)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((release) => {
+        if (this.selected() === undefined && release !== null) this.selected.set(release);
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next(undefined);
+  }
+
   search() {
-    if (this.pristine && this.selected() === undefined) {
-      this.selected.set(this.upcoming());
-      this.pristine = false;
-    }
     this.filterEvent.emit({ filter: this.appName, releaseId: this.selected() });
   }
 }
