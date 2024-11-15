15d18b61c75401142169ada0d86ab99e1e139191
Berivan Bertagna <bbertagna@puzzle.ch>
2024-11-15 10:15:21 +0100

    feat: add edit resource link-button

--- AMW_angular/io/src/app/resources/resources-list/resources-list.component.ts
+++ AMW_angular/io/src/app/resources/resources-list/resources-list.component.ts
@@ -1,12 +1,14 @@
 import { Component, input } from '@angular/core';
 import { Resource } from '../../resource/resource';
 import { ResourceType } from '../../resource/resource-type';
+import { ButtonComponent } from '../../shared/button/button.component';
+import { IconComponent } from '../../shared/icon/icon.component';
 
 @Component({
   selector: 'app-resources-list',
   standalone: true,
   templateUrl: './resources-list.component.html',
-  imports: [],
+  imports: [ButtonComponent, IconComponent],
 })
 export class ResourcesListComponent {
   resourceType = input.required<ResourceType>();
