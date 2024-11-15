8069a8bef1a01220fa0b5394f26eea7e1f2130f4
Berivan Bertagna <bbertagna@puzzle.ch>
2024-11-15 10:33:17 +0100

    fix: fix test of resources-list component

--- AMW_angular/io/src/app/resources/resources-list/resources-list.component.spec.ts
+++ AMW_angular/io/src/app/resources/resources-list/resources-list.component.spec.ts
@@ -1,18 +1,33 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ResourcesListComponent } from './resources-list.component';
+import { ComponentRef } from '@angular/core';
+import { Resource } from '../../resource/resource';
+import { ResourceType } from '../../resource/resource-type';
 
 describe('ResourcesListComponent', () => {
   let component: ResourcesListComponent;
+  let componentRef: ComponentRef<ResourcesListComponent>;
   let fixture: ComponentFixture<ResourcesListComponent>;
+  const resourceType: ResourceType = {
+    id: 1,
+    name: 'type',
+    hasChildren: false,
+    children: [],
+  };
+
+  const resourceGroupsOfResourceType: Resource[] = [];
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [],
+      imports: [ResourcesListComponent],
       providers: [],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ResourcesListComponent);
     component = fixture.componentInstance;
+    componentRef = fixture.componentRef;
+    componentRef.setInput('resourceType', resourceType);
+    componentRef.setInput('resourceGroupList', resourceGroupsOfResourceType);
     fixture.detectChanges();
   });
 
