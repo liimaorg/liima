4bddb3ef6eebf1b205dac42a57384beb7d9f0042
Beri <37876310+BeriBoss@users.noreply.github.com>
2024-11-19 14:28:36 +0100

    feat: change routing for settings (#799)
    * feat: change routing for settings
    
    * refactor: routing settings
    
    * refactor: remove old ui-stuff related to settings
    
    ---------
    
    Co-authored-by: Stephan Girod <girod@puzzle.ch>

--- AMW_angular/io/src/app/settings/settings.component.spec.ts
+++ AMW_angular/io/src/app/settings/settings.component.spec.ts
@@ -1,9 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { RouterTestingModule } from '@angular/router/testing';
 import { SettingsComponent } from './settings.component';
-import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
-import { HttpClient, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 describe('SettingsComponent', () => {
   let component: SettingsComponent;
