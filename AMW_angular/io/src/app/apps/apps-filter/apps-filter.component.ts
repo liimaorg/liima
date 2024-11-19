7f853c043862de2dcc058bf948d5d1b1f0e0fd12
Max Burri <burri@puzzle.ch>
2024-11-19 09:25:01 +0100

    fix: simplify component with childstate
    see: https://youtu.be/aKxcIQMWSNU

--- AMW_angular/io/src/app/apps/apps-filter/apps-filter.component.ts
+++ AMW_angular/io/src/app/apps/apps-filter/apps-filter.component.ts
@@ -1,46 +1,33 @@
-import { ChangeDetectionStrategy, Component, computed, input, Input, OnDestroy, OnInit, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, input, output, signal } from '@angular/core';
 
 import { NgSelectModule } from '@ng-select/ng-select';
 import { Release } from '../../settings/releases/release';
 import { FormsModule } from '@angular/forms';
-import { Output, EventEmitter } from '@angular/core';
 import { ButtonComponent } from '../../shared/button/button.component';
-import { IconComponent } from '../../shared/icon/icon.component';
-import { toObservable } from '@angular/core/rxjs-interop';
-import { skip, Subject, take } from 'rxjs';
-import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-apps-filter',
   standalone: true,
-  imports: [FormsModule, NgSelectModule, ButtonComponent, IconComponent],
+  imports: [FormsModule, NgSelectModule, ButtonComponent],
   templateUrl: './apps-filter.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppsFilterComponent implements OnDestroy {
-  @Input() releases: Release[];
-
-  private destroy$ = new Subject<void>();
+export class AppsFilterComponent {
+  releases = input.required<Release[]>();
   upcoming = input.required<number>();
-  selected = signal<number>(undefined);
 
-  @Output() filterEvent = new EventEmitter<{ filter: string; releaseId: number }>();
+  selection = computed(() => {
+    return {
+      options: this.releases(),
+      selected: signal(this.upcoming()),
+    };
+  });
 
-  appName: string;
+  appName = signal<string>('');
 
-  constructor() {
-    toObservable(this.upcoming)
-      .pipe(takeUntil(this.destroy$))
-      .subscribe((release) => {
-        if (this.selected() === undefined && release !== null) this.selected.set(release);
-      });
-  }
-
-  ngOnDestroy(): void {
-    this.destroy$.next(undefined);
-  }
+  filterEvent = output<{ filter: string; releaseId: number }>();
 
   search() {
-    this.filterEvent.emit({ filter: this.appName, releaseId: this.selected() });
+    this.filterEvent.emit({ filter: this.appName(), releaseId: this.selection().selected() });
   }
 }
