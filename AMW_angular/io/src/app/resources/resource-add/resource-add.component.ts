80ff68b7c57db2d59ccdf0775315221ecab97717
Berivan Bertagna <bbertagna@puzzle.ch>
2024-11-27 10:49:08 +0100

    feat: add resource-add-button and resource-add-modal

---
+++ AMW_angular/io/src/app/resources/resource-add/resource-add.component.ts
@@ -0,0 +1,33 @@
+import { ChangeDetectionStrategy, Component, inject, Input } from '@angular/core';
+import { NgbActiveModal, NgbTypeahead } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
+import { ModalHeaderComponent } from '../../shared/modal-header/modal-header.component';
+import { ButtonComponent } from '../../shared/button/button.component';
+import { ResourceType } from '../../resource/resource-type';
+import { NgSelectModule } from '@ng-select/ng-select';
+
+@Component({
+  selector: 'app-resource-add',
+  standalone: true,
+  imports: [FormsModule, ModalHeaderComponent, ButtonComponent, NgbTypeahead, NgSelectModule],
+  templateUrl: './resource-add.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ResourceAddComponent {
+  activeModal = inject(NgbActiveModal);
+  @Input() resourceType: ResourceType;
+  resourceName: string;
+
+  getTitle() {
+    if (!this.resourceType) return;
+    return this.resourceType.name ? `Create new instance for ${this.resourceType.name}` : `Create new instance`;
+  }
+
+  save() {
+    this.activeModal.close();
+  }
+
+  cancel() {
+    this.activeModal.close();
+  }
+}
--- AMW_angular/io/src/app/resources/resources-list/resources-list.component.ts
+++ AMW_angular/io/src/app/resources/resources-list/resources-list.component.ts
@@ -1,8 +1,10 @@
-import { Component, input } from '@angular/core';
+import { Component, inject, input, output } from '@angular/core';
 import { Resource } from '../../resource/resource';
 import { ResourceType } from '../../resource/resource-type';
 import { ButtonComponent } from '../../shared/button/button.component';
 import { IconComponent } from '../../shared/icon/icon.component';
+import { NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
+import { ResourceAddComponent } from '../resource-add/resource-add.component';
 
 @Component({
   selector: 'app-resources-list',
@@ -11,6 +13,12 @@ import { IconComponent } from '../../shared/icon/icon.component';
   imports: [ButtonComponent, IconComponent],
 })
 export class ResourcesListComponent {
+  private modalService = inject(NgbModal);
   resourceType = input.required<ResourceType>();
   resourceGroupList = input<Resource[]>();
+
+  addResource() {
+    const modalRef: NgbModalRef = this.modalService.open(ResourceAddComponent);
+    modalRef.componentInstance.resourceType = this.resourceType();
+  }
 }
