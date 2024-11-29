f53ef90b7644c50fb0a30697f922e28fc3dd6ece
Berivan Bertagna <bbertagna@puzzle.ch>
2024-11-29 13:30:15 +0100

    fix: fix rebase mistake

--- AMW_business/src/main/java/ch/puzzle/itc/mobiliar/business/resourcegroup/boundary/ResourceTypeLocator.java
+++ AMW_business/src/main/java/ch/puzzle/itc/mobiliar/business/resourcegroup/boundary/ResourceTypeLocator.java
@@ -62,8 +62,4 @@ public class ResourceTypeLocator {
                 .filter(e -> e.getParentResourceType() == null && !ResourceType.createByResourceType(e, null).isDefaultResourceType())
                 .collect(Collectors.toList());
     }
-
-    public boolean hasChildren(Integer resourceTypeId) {
-        return resourceTypeDomainService.hasChildren(resourceTypeId);
-    }
 }
