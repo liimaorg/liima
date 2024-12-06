5d7a891912556ebc9c0bde15ce5a60ecba0ffd37
Stephan Girod <girod@puzzle.ch>
2024-12-06 17:11:18 +0100

    feat: load resource by query param

--- AMW_angular/io/src/app/resources/resource-edit/resource-edit.component.ts
+++ AMW_angular/io/src/app/resources/resource-edit/resource-edit.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, inject, signal } from '@angular/core';
+import { Component, computed, inject, Signal } from '@angular/core';
 import { LoadingIndicatorComponent } from '../../shared/elements/loading-indicator.component';
 import { PageComponent } from '../../layout/page/page.component';
 import { AuthService } from '../../auth/auth.service';
@@ -6,20 +6,23 @@ import { ActivatedRoute } from '@angular/router';
 import { distinctUntilChanged, map } from 'rxjs/operators';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { ResourceService } from '../../resource/resource.service';
+import { TileComponent } from '../../shared/tile/tile.component';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Resource } from '../../resource/resource';
 
 @Component({
   selector: 'app-resources-edit',
   standalone: true,
-  imports: [LoadingIndicatorComponent, PageComponent],
+  imports: [LoadingIndicatorComponent, PageComponent, TileComponent],
   templateUrl: './resource-edit.component.html',
 })
 export class ResourceEditComponent {
   private authService = inject(AuthService);
+  private modalService = inject(NgbModal);
   private resourceService = inject(ResourceService);
   private route = inject(ActivatedRoute);
 
-  isLoading = signal(false);
-
+  resource: Signal<Resource> = this.resourceService.resource;
   resourceId = toSignal(
     this.route.paramMap.pipe(
       map((params) => +params.get('id')),
@@ -28,9 +31,10 @@ export class ResourceEditComponent {
     -1,
   );
 
-  resource = computed(() => {
+  isLoading = computed(() => {
     if (this.resourceId() > -1) {
-      return { id: this.resourceId(), name: 'MyResource' };
+      this.resourceService.getResource(this.resourceId());
+      return false;
     }
   });
 }
