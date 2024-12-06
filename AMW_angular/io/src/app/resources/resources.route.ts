ee73576db345f4b9bc606dc1b1cd94f722753819
Stephan Girod <girod@puzzle.ch>
2024-12-06 13:48:49 +0100

    feat: add resource-edit page

--- AMW_angular/io/src/app/resources/resources.route.ts
+++ AMW_angular/io/src/app/resources/resources.route.ts
@@ -1,3 +1,7 @@
 import { ResourcesPageComponent } from './resources-page.component';
+import { ResourceEditComponent } from './resource-edit/resource-edit.component';
 
-export const resourcesRoute = [{ path: 'resources', component: ResourcesPageComponent }];
+export const resourcesRoute = [
+  { path: 'resources', component: ResourcesPageComponent },
+  { path: 'resource/edit/:id', component: ResourceEditComponent },
+];
