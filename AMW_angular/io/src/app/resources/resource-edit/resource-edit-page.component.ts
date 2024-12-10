51ee49ae4e407dae3e8c7caa91608d49316cc571
Stephan Girod <girod@puzzle.ch>
2024-12-10 09:59:05 +0100

    refactor: remove unused stuff and rename component

--- AMW_angular/io/src/app/resources/resource-edit/resource-edit-page.component.ts
+++ AMW_angular/io/src/app/resources/resource-edit/resource-edit-page.component.ts
@@ -1,23 +1,19 @@
 import { Component, computed, inject, Signal } from '@angular/core';
 import { LoadingIndicatorComponent } from '../../shared/elements/loading-indicator.component';
 import { PageComponent } from '../../layout/page/page.component';
-import { AuthService } from '../../auth/auth.service';
 import { ActivatedRoute } from '@angular/router';
 import { distinctUntilChanged, map } from 'rxjs/operators';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { ResourceService } from '../../resource/resource.service';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { Resource } from '../../resource/resource';
 
 @Component({
   selector: 'app-resources-edit',
   standalone: true,
   imports: [LoadingIndicatorComponent, PageComponent],
-  templateUrl: './resource-edit.component.html',
+  templateUrl: './resource-edit-page.component.html',
 })
-export class ResourceEditComponent {
-  private authService = inject(AuthService);
-  private modalService = inject(NgbModal);
+export class ResourceEditPageComponent {
   private resourceService = inject(ResourceService);
   private route = inject(ActivatedRoute);
 
