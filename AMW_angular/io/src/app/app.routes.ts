e150b3b8684a71a124f4d6580e535c53cf2f1fa8
Stephan Girod <girod@puzzle.ch>
2024-12-18 11:11:14 +0100

    feat: start from angular-artifact

--- AMW_angular/io/src/app/app.routes.ts
+++ AMW_angular/io/src/app/app.routes.ts
@@ -1,5 +1,4 @@
 import { Routes } from '@angular/router';
-import { DeploymentsComponent } from './deployments/deployments.component';
 import { appsRoutes } from './apps/apps.routes';
 import { auditviewRoutes } from './auditview/auditview.routes';
 import { deploymentRoutes } from './deployment/deployment-routes';
@@ -7,10 +6,11 @@ import { settingsRoutes } from './settings/settings.routes';
 import { deploymentsRoutes } from './deployments/deployments.routes';
 import { serversRoute } from './servers/servers.route';
 import { resourcesRoute } from './resources/resources.route';
+import { AppsComponent } from './apps/apps.component';
 
 export const routes: Routes = [
   // default route only, the rest is done in module routing
-  { path: '', component: DeploymentsComponent },
+  { path: '', component: AppsComponent },
 
   ...appsRoutes,
   ...serversRoute,
