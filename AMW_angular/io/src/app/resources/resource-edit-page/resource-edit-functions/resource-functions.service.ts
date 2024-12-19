c89c05ec2bce47dd23f589161b10cb4c174d1e06
Stephan Girod <girod@puzzle.ch>
2024-12-19 12:47:36 +0100

    feat: add permissions and provide them to tile

--- AMW_angular/io/src/app/resources/resource-edit-page/resource-edit-functions/resource-functions.service.ts
+++ AMW_angular/io/src/app/resources/resource-edit-page/resource-edit-functions/resource-functions.service.ts
@@ -2,24 +2,36 @@ import { BaseService } from '../../../base/base.service';
 import { Injectable, signal } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { ResourceFunction } from './resource-function';
-import { catchError } from 'rxjs/operators';
+import { catchError, shareReplay, switchMap } from 'rxjs/operators';
+import { Observable, startWith, Subject } from 'rxjs';
+import { Resource } from '../../../resource/resource';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Injectable({ providedIn: 'root' })
 export class ResourceFunctionsService extends BaseService {
   private path = `${this.getBaseUrl()}/resources/functions`;
+  private functions$: Subject<Number> = new Subject<Number>();
 
-  functions = signal<ResourceFunction[]>([]);
+  private functionById$: Observable<ResourceFunction[]> = this.functions$.pipe(
+    switchMap((id: number) => this.getResourceFunctions(id)),
+    shareReplay(1),
+  );
+
+  functions = toSignal(this.functionById$, { initialValue: [] });
 
   constructor(private http: HttpClient) {
     super();
   }
 
-  getResourceFunctions(id: number): void {
-    this.http
+  setIdForFunctionList(id: number) {
+    this.functions$.next(id);
+  }
+
+  getResourceFunctions(id: number): Observable<ResourceFunction[]> {
+    return this.http
       .get<ResourceFunction[]>(`${this.path}/${id}`, {
         headers: this.getHeaders(),
       })
-      .pipe(catchError(this.handleError))
-      .subscribe((result) => this.functions.set(result));
+      .pipe(catchError(this.handleError));
   }
 }
