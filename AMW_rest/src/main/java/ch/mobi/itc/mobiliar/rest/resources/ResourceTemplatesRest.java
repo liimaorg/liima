82b9f947947527625674ff858ab4d0223187c3e7
llorentelemmc <llorente@puzzle.ch>
2025-01-03 12:38:21 +0000

    feat: endpoints for resource and resource type templates

--- AMW_rest/src/main/java/ch/mobi/itc/mobiliar/rest/resources/ResourceTemplatesRest.java
+++ AMW_rest/src/main/java/ch/mobi/itc/mobiliar/rest/resources/ResourceTemplatesRest.java
@@ -36,7 +36,9 @@ import javax.ws.rs.core.Response;
 
 import ch.mobi.itc.mobiliar.rest.dtos.TemplateDTO;
 import ch.puzzle.itc.mobiliar.business.resourcegroup.boundary.ResourceGroupLocator;
+import ch.puzzle.itc.mobiliar.business.resourcegroup.entity.ResourceEntity;
 import ch.puzzle.itc.mobiliar.business.resourcegroup.entity.ResourceGroupEntity;
+import ch.puzzle.itc.mobiliar.business.resourcegroup.entity.ResourceTypeEntity;
 import ch.puzzle.itc.mobiliar.business.template.boundary.TemplateEditor;
 import ch.puzzle.itc.mobiliar.business.template.control.TemplatesScreenDomainService;
 import ch.puzzle.itc.mobiliar.business.template.entity.TemplateDescriptorEntity;
@@ -46,14 +48,15 @@ import ch.puzzle.itc.mobiliar.common.exception.TemplateNotDeletableException;
 import ch.puzzle.itc.mobiliar.common.exception.ValidationException;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import javax.persistence.EntityManager;
 
 /**
- * Rest boundary for Resource-Templates
+ * Rest boundary for Resource and Resource Type Templates
  */
 @RequestScoped
-@Path("/resources/{resourceGroupName}/{releaseName}/templates")
-@Api(value = "/resources/{resourceGroupName}/{releaseName}/templates", description = "Resource templates")
-public class ResourceTemplatesRest {;
+@Path("/resources/templates")
+@Api(value = "/resources/templates")
+public class ResourceTemplatesRest {
 
     @Inject
     ResourceGroupLocator resourceGroupLocator;
@@ -64,7 +67,11 @@ public class ResourceTemplatesRest {;
     @Inject
     TemplateEditor templateEditor;
 
+    @Inject
+    private EntityManager entityManager;
+
     @GET
+    @Path("/{resourceGroupName}/{releaseName}")
     @ApiOperation(value = "Get all templates for a resource in a specific release")
     public List<TemplateDTO> getResourceTemplates(@PathParam("resourceGroupName") String resourceGroupName,
                                                 @PathParam("releaseName") String releaseName) throws ValidationException {
@@ -79,7 +86,40 @@ public class ResourceTemplatesRest {;
     }
 
     @GET
-    @Path("/{templateName}")
+    @Path("/{resourceId}")
+    @ApiOperation(value = "Get all templates for a resource")
+    public List<TemplateDTO> getResourceTemplatesByResourceId(@PathParam("resourceId") Integer resourceId) throws NotFoundException {
+        ResourceEntity resource = entityManager.find(ResourceEntity.class, resourceId);
+        if (resource == null) {
+            throw new NotFoundException("Resource not found");
+        }
+        List<TemplateDescriptorEntity> templates = templateService.getGlobalTemplateDescriptorsForResource(resource);
+        List<TemplateDTO> templateDTOs = new ArrayList<>();
+        for (TemplateDescriptorEntity template : templates) {
+            templateDTOs.add(new TemplateDTO(template));
+        }
+        return templateDTOs;
+    }
+
+    @GET
+    @Path("/resourceType/{resourceTypeId}")
+    @ApiOperation(value = "Get all templates for a resource type")
+    public Response getResourceTypeTemplatesById(@PathParam("resourceTypeId") Integer resourceTypeId) throws NotFoundException {
+
+        ResourceTypeEntity resourceType = entityManager.find(ResourceTypeEntity.class, resourceTypeId);
+        if (resourceType == null) {
+            throw new NotFoundException("Resource type not found");
+        }
+        List<TemplateDescriptorEntity> templates = templateService.getGlobalTemplateDescriptorsForResourceType(resourceType);
+        List<TemplateDTO> templateDTOs = new ArrayList<>();
+        for (TemplateDescriptorEntity template : templates) {
+            templateDTOs.add(new TemplateDTO(template));
+        }
+        return Response.status(Response.Status.OK).entity(templateDTOs).build();
+    }
+
+    @GET
+    @Path("/{resourceGroupName}/{releaseName}/{templateName}")
     @ApiOperation(value = "Get a template for a resource in a specific release")
     public TemplateDTO getResourceTemplate(@PathParam("resourceGroupName") String resourceGroupName,
                                                  @PathParam("releaseName") String releaseName,
@@ -92,7 +132,7 @@ public class ResourceTemplatesRest {;
     }
 
     @DELETE
-    @Path("/{templateName}")
+    @Path("/{resourceGroupName}/{releaseName}/{templateName}")
     @ApiOperation(value = "Delete a template for a resource in a specific release")
     public Response deleteResourceTemplate(@PathParam("resourceGroupName") String resourceGroupName,
                                            @PathParam("releaseName") String releaseName,
@@ -106,6 +146,7 @@ public class ResourceTemplatesRest {;
     }
 
     @POST
+    @Path("/{resourceGroupName}/{releaseName}")
     @ApiOperation(value = "Create a template for a resource in a specific release")
     public Response createResourceTemplates(@PathParam("resourceGroupName") String resourceGroupName,
                                             @PathParam("releaseName") String releaseName,
@@ -119,7 +160,7 @@ public class ResourceTemplatesRest {;
     }
 
     @PUT
-    @Path("/{templateName}")
+    @Path("/{resourceGroupName}/{releaseName}/{templateName}")
     @ApiOperation(value = "Update a template for a resource in a specific release")
     public Response updateResourceTemplates(@PathParam("resourceGroupName") String resourceGroupName,
                                             @PathParam("releaseName") String releaseName,
