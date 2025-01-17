ac8d4aa7be1118bb9c5a62feec597f0430297674
Clara Llorente Lemm <71637828+llorentelemmc@users.noreply.github.com>
2025-01-17 13:05:00 +0100

    Settings page fixes: tab naming, environments page selection (#830)

--- AMW_angular/io/src/app/deployment/deployment-routes.ts
+++ AMW_angular/io/src/app/deployment/deployment-routes.ts
@@ -1,10 +1,11 @@
 import { DeploymentComponent } from './deployment.component';
 
 export const deploymentRoutes = [
-  { path: 'deployment', component: DeploymentComponent },
-  { path: 'deployment/:deploymentId', component: DeploymentComponent },
+  { path: 'deployment', component: DeploymentComponent, title: 'Deployment - Liima' },
+  { path: 'deployment/:deploymentId', component: DeploymentComponent, title: 'Deployment - Liima' },
   {
     path: 'deployment/:appserverName/:releaseName',
     component: DeploymentComponent,
+    title: 'Deployment - Liima',
   },
 ];
