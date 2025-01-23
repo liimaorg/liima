71ede441c5b35bdae31eca636d3a86735be70d99
Berivan Bertagna <bbertagna@puzzle.ch>
2025-01-23 11:14:16 +0100

    feat: replaced the existing app info tables with the reusable one

--- AMW_angular/io/src/app/settings/application-info/application-info.component.ts
+++ AMW_angular/io/src/app/settings/application-info/application-info.component.ts
@@ -5,11 +5,12 @@ import { SettingService } from '../../setting/setting.service';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { AppInformation } from '../../setting/app-information';
 import { AppConfiguration } from '../../setting/app-configuration';
+import { TableComponent, TableHeader } from '../../shared/table/table.component';
 
 @Component({
   selector: 'app-application-info',
   standalone: true,
-  imports: [LoadingIndicatorComponent, AsyncPipe],
+  imports: [LoadingIndicatorComponent, AsyncPipe, TableComponent],
   templateUrl: './application-info.component.html',
 })
 export class ApplicationInfoComponent {
@@ -17,4 +18,39 @@ export class ApplicationInfoComponent {
   appVersions = toSignal(this.settingService.getAppInformation(), { initialValue: [] as AppInformation[] });
   appConfigs = toSignal(this.settingService.getAllAppSettings(), { initialValue: [] as AppConfiguration[] });
   isLoading = computed(() => !this.appVersions().length || !this.appConfigs().length);
+
+  appVersionsHeader(): TableHeader[] {
+    return [
+      {
+        key: 'key',
+        title: 'Key',
+      },
+      {
+        key: 'value',
+        title: 'Value',
+      },
+    ];
+  }
+
+  appConfigsHeader(): TableHeader[] {
+    return [
+      {
+        key: 'key',
+        title: 'key',
+        type: 'split',
+        nested: [
+          { key: 'value', title: 'Key' },
+          { key: 'env', title: 'ENV' },
+        ],
+      },
+      {
+        key: 'value',
+        title: 'Value',
+      },
+      {
+        key: 'defaultValue',
+        title: 'Default value',
+      },
+    ];
+  }
 }
