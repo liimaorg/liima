4ee944054f804cd48b27f98104527fef55e5efee
Berivan Bertagna <bbertagna@puzzle.ch>
2025-02-18 16:52:51 +0100

    fix: fix tests

--- AMW_angular/io/src/app/apps/apps-list/apps-list.component.spec.ts
+++ AMW_angular/io/src/app/apps/apps-list/apps-list.component.spec.ts
@@ -1,9 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AppsListComponent } from './apps-list-component';
+import { ComponentRef } from '@angular/core';
 
 describe('AppsListComponent', () => {
   let component: AppsListComponent;
+  let componentRef: ComponentRef<AppsListComponent>;
   let fixture: ComponentFixture<AppsListComponent>;
 
   beforeEach(async () => {
@@ -13,6 +15,9 @@ describe('AppsListComponent', () => {
 
     fixture = TestBed.createComponent(AppsListComponent);
     component = fixture.componentInstance;
+    componentRef = fixture.componentRef;
+    componentRef.setInput('apps', []);
+    componentRef.setInput('even', true);
     fixture.detectChanges();
   });
 
