5cc352bdf75db63a89d9b305926fb905f717aaa9
Berivan Bertagna <bbertagna@puzzle.ch>
2025-03-04 16:45:51 +0100

    refactor: select revision in resource-template-edit comp and spec

--- AMW_angular/io/src/app/resources/resource-edit/resource-templates/resource-template-edit.component.spec.ts
+++ AMW_angular/io/src/app/resources/resource-edit/resource-templates/resource-template-edit.component.spec.ts
@@ -2,10 +2,23 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ResourceTemplateEditComponent } from './resource-template-edit.component';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { ComponentRef } from '@angular/core';
+import { ResourceTemplate } from '../../../resource/resource-template';
 
 describe('ResourceTemplateEditComponent', () => {
   let component: ResourceTemplateEditComponent;
+  let componentRef: ComponentRef<ResourceTemplateEditComponent>;
   let fixture: ComponentFixture<ResourceTemplateEditComponent>;
+  const template: ResourceTemplate = {
+    id: null,
+    relatedResourceIdentifier: '',
+    name: '',
+    targetPath: '',
+    targetPlatforms: [''],
+    fileContent: '',
+    sourceType: 'RESOURCE',
+    version: 1,
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -15,18 +28,8 @@ describe('ResourceTemplateEditComponent', () => {
 
     fixture = TestBed.createComponent(ResourceTemplateEditComponent);
     component = fixture.componentInstance;
-
-    component.template = {
-      id: null,
-      relatedResourceIdentifier: '',
-      name: '',
-      targetPath: '',
-      targetPlatforms: [''],
-      fileContent: '',
-      sourceType: 'RESOURCE',
-      version: 1,
-    };
-
+    componentRef = fixture.componentRef;
+    componentRef.setInput('template', template);
     fixture.detectChanges();
   });
 
