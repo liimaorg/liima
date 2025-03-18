93fc1c22ca69683cd1746f443acf78aa159221d9
llorentelemmc <llorente@puzzle.ch>
2025-03-18 16:02:54 +0100

    fix: alphabetical sorting of resource and resource type templates

--- AMW_angular/io/src/app/resources/resource-type-edit/resource-type-templates/resource-type-templates-list.component.ts
+++ AMW_angular/io/src/app/resources/resource-type-edit/resource-type-templates/resource-type-templates-list.component.ts
@@ -96,11 +96,13 @@ export class ResourceTypeTemplatesListComponent implements OnDestroy {
   }
 
   mapListEntries(templates: ResourceTemplate[]) {
-    return templates.map((template) => ({
-      name: template.name,
-      description: template.targetPath,
-      id: template.id,
-    }));
+    return templates
+      .sort((a, b) => a.name.localeCompare(b.name))
+      .map((template) => ({
+        name: template.name,
+        description: template.targetPath,
+        id: template.id,
+      }));
   }
 
   addTemplate() {
