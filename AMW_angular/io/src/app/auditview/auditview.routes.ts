eb49247c7d9ea5a833d279a6661742953accba66
llorentelemmc <llorente@puzzle.ch>
2025-04-01 10:38:24 +0200

    Use constant in routes

--- AMW_angular/io/src/app/auditview/auditview.routes.ts
+++ AMW_angular/io/src/app/auditview/auditview.routes.ts
@@ -1,6 +1,7 @@
 import { AuditviewComponent } from './auditview.component';
 
+const AUDIT_VIEW_TITLE = 'Audit View - Liima';
 export const auditviewRoutes = [
-  { path: 'auditview', component: AuditviewComponent, title: 'Audit View - Liima' },
-  { path: 'auditview/:resourceId', component: AuditviewComponent, title: 'Audit View - Liima' },
+  { path: 'auditview', component: AuditviewComponent, title: AUDIT_VIEW_TITLE },
+  { path: 'auditview/:resourceId', component: AuditviewComponent, title: AUDIT_VIEW_TITLE },
 ];
