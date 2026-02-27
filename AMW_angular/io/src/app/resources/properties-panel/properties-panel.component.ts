fb1dc5d6d80d25e49f2ba81003e3dd9a8aa24533
Stephan Girod <girod@puzzle.ch>
2026-02-27 09:42:08 +0100

    feat: prevent saving on validation errors and add hint

--- AMW_angular/io/src/app/resources/properties-panel/properties-panel.component.ts
+++ AMW_angular/io/src/app/resources/properties-panel/properties-panel.component.ts
@@ -1,10 +1,11 @@
 import { Component, input, output } from '@angular/core';
 import { ButtonComponent } from '../../shared/button/button.component';
+import { NgbTooltip } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-properties-panel',
   standalone: true,
-  imports: [ButtonComponent],
+  imports: [ButtonComponent, NgbTooltip],
   templateUrl: './properties-panel.component.html',
   styleUrl: './properties-panel.component.scss',
 })
@@ -12,6 +13,7 @@ export class PropertiesPanelComponent {
   canEdit = input<boolean>(false);
   isSaving = input<boolean>(false);
   hasChanges = input<boolean>(false);
+  hasValidationErrors = input<boolean>(false);
   errorMessage = input<string | null>(null);
   successMessage = input<string | null>(null);
 
