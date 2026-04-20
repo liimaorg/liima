442a6f137b98ae96f94471326756e5a4b865dfff
Max Burri <burri@puzzle.ch>
2026-04-20 13:15:58 +0200

    fix: cleanup after rebase

--- AMW_business/src/main/java/ch/puzzle/itc/mobiliar/business/resourcegroup/boundary/GetResourceUseCase.java
+++ AMW_business/src/main/java/ch/puzzle/itc/mobiliar/business/resourcegroup/boundary/GetResourceUseCase.java
@@ -1,6 +1,7 @@
 package ch.puzzle.itc.mobiliar.business.resourcegroup.boundary;
 
 import ch.puzzle.itc.mobiliar.business.resourcegroup.entity.ResourceEntity;
+import ch.puzzle.itc.mobiliar.common.exception.ResourceNotFoundException;
 
 public interface GetResourceUseCase {
 
