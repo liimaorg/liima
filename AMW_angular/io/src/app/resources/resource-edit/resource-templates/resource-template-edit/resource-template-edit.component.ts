f35685f5056c8f2d17e008a9cb1e839c1a009c60
Beri <37876310+BeriBoss@users.noreply.github.com>
2026-08-03 10:31:26 +0200

    fix: reset form when canceling edit template (#1031)
    * fix: reset form when canceling edit template
    
    * refactor: rename field

--- AMW_angular/io/src/app/resources/resource-edit/resource-templates/resource-template-edit/resource-template-edit.component.ts
+++ AMW_angular/io/src/app/resources/resource-edit/resource-templates/resource-template-edit/resource-template-edit.component.ts
@@ -47,15 +47,17 @@ interface TargetPlatformModel {
 export class ResourceTemplateEditComponent {
   activeModal = inject(NgbActiveModal);
 
-  private _template!: ResourceTemplate;
+  private _currentWorkingTemplate!: ResourceTemplate;
   @Input() set template(value: ResourceTemplate) {
-    this._template = value;
     if (value && value.id) {
+      this._currentWorkingTemplate = { ...value, targetPlatforms: [...value.targetPlatforms] };
       this.loadRevisions(value.id);
+    } else {
+      this._currentWorkingTemplate = value;
     }
   }
   get template(): ResourceTemplate {
-    return this._template;
+    return this._currentWorkingTemplate;
   }
 
   private readonly canAddOrEditSignal = signal(false);
@@ -103,8 +105,8 @@ export class ResourceTemplateEditComponent {
 
   save() {
     if (this.isValidForm()) {
-      if (this.revision()) this.template.fileContent = this.diffValue.original;
-      this.saveTemplate.emit(this.template);
+      if (this.revision()) this._currentWorkingTemplate.fileContent = this.diffValue.original;
+      this.saveTemplate.emit(this._currentWorkingTemplate);
       this.activeModal.close();
     }
   }
@@ -117,7 +119,7 @@ export class ResourceTemplateEditComponent {
     return allTargetPlatforms.map((name) => {
       return {
         name: name,
-        selected: this.template.targetPlatforms.includes(name),
+        selected: this._currentWorkingTemplate.targetPlatforms.includes(name),
       };
     });
   }
@@ -134,11 +136,13 @@ export class ResourceTemplateEditComponent {
   }
 
   selectTargetPlatform(targetPlatform: TargetPlatformModel) {
-    if (!this.template.targetPlatforms.includes(targetPlatform.name)) {
-      this.template.targetPlatforms.push(targetPlatform.name);
+    if (!this._currentWorkingTemplate.targetPlatforms.includes(targetPlatform.name)) {
+      this._currentWorkingTemplate.targetPlatforms.push(targetPlatform.name);
     } else {
-      const indexOfTargetPlatformNameToRemove = this.template.targetPlatforms.indexOf(targetPlatform.name);
-      this.template.targetPlatforms.splice(indexOfTargetPlatformNameToRemove, 1);
+      const indexOfTargetPlatformNameToRemove = this._currentWorkingTemplate.targetPlatforms.indexOf(
+        targetPlatform.name,
+      );
+      this._currentWorkingTemplate.targetPlatforms.splice(indexOfTargetPlatformNameToRemove, 1);
     }
   }
 
@@ -150,11 +154,16 @@ export class ResourceTemplateEditComponent {
 
   selectRevision(revisionId: number | null, displayName: string | null): void {
     if (revisionId && displayName) {
-      this.templatesService.getTemplateByIdAndRevision(this.template.id!, revisionId).subscribe((revision) => {
-        this.revision.set(revision);
-        this.selectedRevisionName = displayName;
-        this.diffValue = { original: this.template.fileContent, modified: this.revision()!.fileContent };
-      });
+      this.templatesService
+        .getTemplateByIdAndRevision(this._currentWorkingTemplate.id!, revisionId)
+        .subscribe((revision) => {
+          this.revision.set(revision);
+          this.selectedRevisionName = displayName;
+          this.diffValue = {
+            original: this._currentWorkingTemplate.fileContent,
+            modified: this.revision()!.fileContent,
+          };
+        });
     } else {
       //reset selected revision
       this.revision.set(null);
@@ -168,13 +177,15 @@ export class ResourceTemplateEditComponent {
 
   isNameValid(): boolean {
     const REGEXP_NON_EMPTY_TRIMMED = /^\S(.*\S)?$/;
-    return this.template ? REGEXP_NON_EMPTY_TRIMMED.test(this.template.name) : false;
+    return this._currentWorkingTemplate ? REGEXP_NON_EMPTY_TRIMMED.test(this._currentWorkingTemplate.name) : false;
   }
 
   isValidTargetPath() {
     const REGEXP_FILE_PATH_PATTERN = /^(?![\w]:|\/|\.\.)(?!.*\.\.)(?=.*\S)[^\s].*[^\s]$|^$/;
-    return this.template && this.template.targetPath && this.template.targetPath.trim() !== ''
-      ? REGEXP_FILE_PATH_PATTERN.test(this.template.targetPath)
+    return this._currentWorkingTemplate &&
+      this._currentWorkingTemplate.targetPath &&
+      this._currentWorkingTemplate.targetPath.trim() !== ''
+      ? REGEXP_FILE_PATH_PATTERN.test(this._currentWorkingTemplate.targetPath)
       : false;
   }
 }
